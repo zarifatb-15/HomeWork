6703da820803f537f98b99767fa7ae7b5c219d4e
zarifatb-15 <zarifatb@code.edu.az>
2025-10-05 07:49:46 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿//taks1
+// int a = 3;
+// int b = 4;
+// int c = 5;
+// if (a * a + b * b == c * c)
+// {
+//     Console.WriteLine("Ucbucaq duzbucaqlirdir");
+// }
+// else
+// {
+//     Console.WriteLine("Ucbucaq duzbucaqli deyil");
+// }
+
+
+
+//taks2
+// string s= "Zarifa";
+// int l = s.Length;
+// if (l % 2 == 1)
+// {
+//     char middle = s[l / 2];
+//     Console.WriteLine(middle);
+// }
+// else
+// {
+//     string middle = s.Substring(l / 2 - 1, 2);
+//     Console.WriteLine(middle);
+// }
+
+
+
+//taks3
+// int num = 378;
+// int sum = 0;
+// while (num > 0)
+// {
+//     int digit = num % 10;
+//     num = num / 10;
+//     if (digit % 2 == 1)
+//     {
+//         sum += digit;
+//     }
+// }
+// Console.WriteLine(sum);
+
+
+
+//taks4
+// int x = 20;
+// int y = 5;
+// for (int i = x; i > y; i--)
+// {
+//     if (i % y == 0)
+//     {
+//         Console.WriteLine();
+//     }
+    
+// }
